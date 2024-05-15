c2f8876d5b96d72734c348c286f40680651cc338
Rolando Rosales <93052733+rolo-g@users.noreply.github.com>
2024-05-15 00:18:35 -0500

    Initial commit

---
+++ piporeg.v
@@ -0,0 +1,12 @@
+//Rolando Rosales 1001850424 - HW8 P2 parameterized 4-bit PI/PO reg
+
+module piporeg #(parameter N = 4)(
+	input [N-1:0] x,
+	input CLK, CLR,
+	output reg [N-1:0] y);
+		always @ (negedge CLK, negedge CLR)
+			begin 
+				if (CLR == 1'b0) y <= 0;
+					else if (CLK == 1'b0) y <= x;
+			end
+endmodule 
---
+++ upcounter.v
@@ -0,0 +1,11 @@
+//Rolando Rosales 1001850424 - HW8 P3 parameterized 4-bit binary up counter
+
+module upcounter #(parameter N = 4) (
+	input INC, CLR, LDR,
+	input [N-1:0] x,
+	output reg [N-1:0] y);
+	always @ (negedge INC, negedge CLR, negedge LDR)
+		if (CLR == 1'b0) y <= 0;
+			else if (LDR == 1'b0) y <= x;
+			else if (INC == 1'b0) y <= y + 1'b1;
+endmodule 
